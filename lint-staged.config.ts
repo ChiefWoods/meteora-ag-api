b44772f0f1efbab8b6131260e907bd01454608f3
Chii Yuen <chii.yuen@hotmail.com>
2026-08-22 14:18:03 +0800

    chore: update lint-staged config

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,3 +1,4 @@
 export default {
-  "**/*.{js,jsx,ts,tsx,mjs,mts,cjs,cts}": ["oxfmt --write", "oxlint --fix"],
+  "**/*.{js,jsx,ts,tsx,mjs,mts,cjs,cts}": ["bun run format", "bun run lint"],
+  "**/*.md": "bun run format",
 };
